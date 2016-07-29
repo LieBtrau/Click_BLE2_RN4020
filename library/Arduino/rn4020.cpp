48c60d0b491dd8061cd8875b3a63269e71388a32
Christoph Tack <prog.send@gmail.com>
2016-07-29 22:31:19 +0200

    removing CTS-functionality adding status updates adding getting handle of UUID service+characteristic
    Signed-off-by: Christoph Tack <prog.send@gmail.com>

--- library/Arduino/rn4020.cpp
+++ library/Arduino/rn4020.cpp
@@ -28,8 +28,7 @@ void UART_Write_Text(unsigned char *_data)
 } // extern "C"
 #endif
 
-rn4020::rn4020(HardwareSerial &s, byte pinCts, byte pinWake_sw, byte pinWake_hw, byte pinEnPwr, byte pinBtActive):
-    _pinCts_14(pinCts),
+rn4020::rn4020(HardwareSerial &s, byte pinWake_sw, byte pinWake_hw, byte pinEnPwr, byte pinBtActive):
     _pinWake_sw_7(pinWake_sw),
     _pinWake_hw_15(pinWake_hw),
     _pinEnPwr(pinEnPwr),
@@ -45,10 +44,8 @@ bool rn4020::begin(unsigned long baudrate)
     pinMode(_pinEnPwr, OUTPUT);
     pinMode(_pinWake_hw_15, OUTPUT);
     pinMode(_pinWake_sw_7, OUTPUT);
-    pinMode(_pinCts_14, OUTPUT);
     pinMode(_pinActive_12, INPUT);
 
-    digitalWrite(_pinCts_14,LOW);
     digitalWrite(_pinWake_hw_15, LOW);
     digitalWrite(_pinWake_sw_7, LOW);
     digitalWrite(_pinEnPwr, HIGH);//module OFF
@@ -56,6 +53,33 @@ bool rn4020::begin(unsigned long baudrate)
     digitalWrite(_pinEnPwr, LOW);//module ON
     digitalWrite(_pinWake_sw_7, HIGH);
     return waitForStartup(baudrate);
+}
+
+bool rn4020::update(rn4020::STATUS_UPDATES& su)
+{
+    char* readLine;
+    su=SU_UNKNOWN;
+    if(!getLine(&readLine))
+    {
+        return false;
+    }
+    if(strstr(readLine, "Connected"))
+    {
+        su=SU_CONNECTED;
+    }
+    if(strstr(readLine, "Connection End"))
+    {
+        su=SU_DISCONNECTED;
+    }
+    if(su==SU_UNKNOWN)
+    {
+        //If no matching string found
+#if DEBUG_LEVEL >= DEBUG_ALL
+        sPortDebug->print("Unknown input: ");
+        sPortDebug->println(readLine);
+#endif
+    }
+    return true;
 }
 
 /* If the module is in an unknown state, e.g. unknown baudrate, then it can only be reset by toggling its
@@ -105,13 +129,13 @@ bool rn4020::set(rn4020::SETGET st, unsigned long ulValue)
 
     switch(st)
     {
-    case SRV_SERVICES:
+    case SG_SRV_SERVICES:
         ble2_set_server_services(ulValue);
         break;
-    case SUP_FEATURES:
+    case SG_SUP_FEATURES:
         ble2_set_supported_features(ulValue);
         break;
-    case BAUDRATE:
+    case SG_BAUDRATE:
         switch(ulValue)
         {
         case 2400:
@@ -139,13 +163,13 @@ bool rn4020::set(rn4020::SETGET st, unsigned long ulValue)
             ble2_set_baud_rate(BR_921600);
             break;
         default:
-            //#if DEBUG_LEVEL >= DEBUG_ALL
-            //            sPortDebug->println("Unknown baud rate");
-            //#endif
+#if DEBUG_LEVEL >= DEBUG_ALL
+            sPortDebug->println("Unknown baud rate");
+#endif
             return false;
         }
         break;
-    case TX_POWER:
+    case SG_TX_POWER:
         if(ulValue>7)
         {
             return false;
@@ -162,7 +186,7 @@ bool rn4020::set(rn4020::SETGET st, unsigned long ulValue)
     {
 #if DEBUG_LEVEL >= DEBUG_ALL
         sPortDebug->println("No response for set");
- #endif
+#endif
         return false;
     }
     return true;
@@ -170,10 +194,10 @@ bool rn4020::set(rn4020::SETGET st, unsigned long ulValue)
 
 bool rn4020::createService(rn4020::SERVICES srv)
 {
-    //Create service
+    //Create service: https://www.bluetooth.com/specifications/gatt/services
     switch(srv)
     {
-    case IAS:
+    case SRV_IAS:
         ble2_set_private_service_uuid("1802");
         break;
     default:
@@ -183,11 +207,11 @@ bool rn4020::createService(rn4020::SERVICES srv)
     {
         return false;
     }
-    //Set characteristics of the created service
+    //Set characteristics of the created service: https://www.bluetooth.com/specifications/gatt/characteristics
     switch(srv)
     {
-    case IAS:
-        ble2_set_private_characteristics("1802","04",1);//"write without response", 1 byte maximum
+    case SRV_IAS:
+        ble2_set_private_characteristics("2A06","04",1);//"write without response", 1 byte maximum
         break;
     default:
         return false;
@@ -209,6 +233,21 @@ bool rn4020::doAdvertizing(bool bStartNotStop, unsigned int interval_ms)
     return waitForReply(2000,"AOK");
 }
 
+bool rn4020::dummy()
+{
+    char handle[10];
+    if(!getHandle("1802","2A06", handle))
+    {
+#if DEBUG_LEVEL >= DEBUG_ALL
+        sPortDebug->println("GetHandle Failed!!");
+#endif
+    }else
+    {
+#if DEBUG_LEVEL >= DEBUG_ALL
+        sPortDebug->println((char*)handle);
+#endif
+    }
+}
 
 bool rn4020::waitForStartup(unsigned long baudrate)
 {
@@ -228,6 +267,71 @@ bool rn4020::waitForStartup(unsigned long baudrate)
     sPortDebug->println("RN4020 Module found");
 #endif
     return true;
+}
+
+bool rn4020::getHandle(const char* service, const char* characteristic, char handle[])
+{
+    ble2_list_server_services();
+    const byte BUFFSIZE=200;
+    char buf[BUFFSIZE];
+    if(!handle)
+    {
+        return false;
+    }
+    if(!waitForReply(2000, "END\r\n", buf, BUFFSIZE))
+    {
+        return false;
+    }
+#if DEBUG_LEVEL >= DEBUG_ALL
+    sPortDebug->println(buf);
+#endif
+    char* pch= strtok (buf,"\r\n");
+    byte state=0;
+    do
+    {
+        switch(state)
+        {
+        case 0:
+            //Nothing found yet, split string in lines;
+            if(strstr(pch,service))
+            {
+                state=1;
+#if DEBUG_LEVEL >= DEBUG_ALL
+                sPortDebug->println("Service found");
+#endif
+            }
+            pch = strtok (NULL, "\r\n");
+            break;
+        case 1:
+            //Service found, now looking for line with characteristic
+            if(strncmp(pch,"  ",2))
+            {
+#if DEBUG_LEVEL >= DEBUG_ALL
+                sPortDebug->println("Characteristic not found in service");
+#endif
+                return false;
+            }
+            if(strstr(pch, characteristic))
+            {
+                state=2;
+            }
+            else
+            {
+                pch = strtok (NULL, "\r\n");
+            }
+            break;
+        case 2:
+            //Characteristic found, now looking for handle
+            pch=strtok(pch,",");
+            pch=strtok(NULL,",");
+            if(pch)
+            {
+                strcpy(handle, pch);
+                return true;
+            }
+        }
+    }while (pch != NULL);
+    return false;
 }
 
 bool rn4020::isModuleActive(unsigned long uiTimeout)
@@ -255,14 +359,38 @@ bool rn4020::isModuleActive(unsigned long uiTimeout)
     return true;
 }
 
+//Read multiple lines and search for pattern
+bool rn4020::waitForReply(unsigned long uiTimeout, const char *pattern, char buf[], byte buffsize)
+{
+    if(!buf)
+    {
+        return false;
+    }
+    buf[0]='\0';
+    unsigned long ulStartTime=millis();
+    byte index=0;
+    do{
+        if(!sPort->available())
+        {
+            continue;
+        }
+        char c=sPort->read();
+        if((((byte)c)!=0xFF) && (((byte)c)!=0x00))
+        {
+            buf[index<buffsize-1 ? index++ : index=0]=c;
+        }
+        buf[index]='\0';
+    }while(millis()<ulStartTime+uiTimeout && !strstr(buf, pattern));
+    return strstr(buf, pattern);
+}
+
 bool rn4020::waitForReply(unsigned long uiTimeout, const char* pattern)
 {
-    char readLine[10]={0};
     unsigned long ulStartTime=millis();
+    char* readLine=0;
     do
     {
-        getLine(uiTimeout, readLine, 10);
-        if(strstr(readLine, pattern))
+        if(getLine(&readLine) && strstr(readLine, pattern))
         {
             return true;
         }
@@ -270,52 +398,33 @@ bool rn4020::waitForReply(unsigned long uiTimeout, const char* pattern)
     return false;
 }
 
-void rn4020::getLine(unsigned long uiTimeout, char* buf, byte buflength)
+bool rn4020::getLine(char** pReadLine)
 {
-    byte index=0;
-    char b=0,c=0;
-    if(!buf)
+    const byte BUFFSIZE=40;
+    static char buf[BUFFSIZE];
+    static byte index=0;
+    static char b=0,c=0;
+    if(!sPort->available())
     {
-        return;
+        return false;
     }
-    buf[0]='\0';
-    unsigned long ulStartTime=millis();
-    while(millis()<ulStartTime+uiTimeout && index<buflength)
+    b=c;
+    c=sPort->read();
+    if((((byte)c)!=0xFF) && (((byte)c)!=0x00))
     {
-        if(sPort->available())
-        {
-            b=c;
-            c=sPort->read();
-            if((((byte)c)!=0xFF) && (((byte)c)!=0x00))
-            {
-                buf[index++]=c;
-            }
-            if(b=='\r' && c=='\n')
-            {
-                break;
-            }
-        }
-    }
-    buf[index]='\0';
+        buf[index<BUFFSIZE-1 ? index++ : index=0]=c;
 #if DEBUG_LEVEL >= DEBUG_ALL
-    if(!index)
-    {
-        sPortDebug->println("Nothing received");
-        return;
-    }
-    sPortDebug->print("RX: ");
-    //Print data in plain text format
-    for(byte i=0;i<index;i++)
-    {
-        sPortDebug->print(buf[i]>27 ? buf[i]: '.');
-    }
-    sPortDebug->print("\t\t");
-    //Print data in HEX format
-    for(byte i=0;i<index;i++)
-    {
-        sPortDebug->print((byte)buf[i],HEX);
-        sPortDebug->print(" ");
-    }
-    sPortDebug->println();
+        char charbuf[20];
+        sprintf(charbuf,"RX: %c \t\t%02x",c>27?c:'.',(byte)c);
+        sPortDebug->println(charbuf);
 #endif
+    }
+    if(b=='\r' && c=='\n')
+    {
+        *pReadLine=buf;
+        buf[index]='\0';
+        index=0;
+        return true;
+    }
+    return false;
 }
--- library/Arduino/rn4020.h
+++ library/Arduino/rn4020.h
@@ -8,30 +8,39 @@ class rn4020
 public:
     typedef enum
     {
-        SRV_SERVICES,
-        SUP_FEATURES,
-        BAUDRATE,
-        TX_POWER
+        SG_SRV_SERVICES,
+        SG_SUP_FEATURES,
+        SG_BAUDRATE,
+        SG_TX_POWER
     }SETGET;
     typedef enum
     {
-        IAS //Immediate Alert Service
+        SRV_IAS //Immediate Alert Service
     }SERVICES;
-    rn4020(HardwareSerial &s, byte pinCts, byte pinWake_sw, byte pinWake_hw, byte pinEnPwr, byte pinBtActive);
+    typedef enum
+    {
+        SU_CONNECTED,
+        SU_DISCONNECTED,
+        SU_UNKNOWN
+    }STATUS_UPDATES;
+    rn4020(HardwareSerial &s, byte pinWake_sw, byte pinWake_hw, byte pinEnPwr, byte pinBtActive);
     bool begin(unsigned long baudrate);
+    bool update(STATUS_UPDATES& su);
     bool set(SETGET st, unsigned long ulValue);
     bool doFactoryDefault();
     bool doReboot(unsigned long baudrate);
     bool doAdvertizing(bool bStartNotStop, unsigned int interval_ms);
     bool createService(SERVICES srv);
+    bool dummy();
 private:
-    void getLine(unsigned long uiTimeout, char *readLine, byte buflength);
+    bool getLine(char **pReadLine);
     bool waitForReply(unsigned long uiTimeout, const char *pattern);
+    bool waitForReply(unsigned long uiTimeout, const char *pattern, char buf[], byte buffsize);
     bool isModuleActive(unsigned long uiTimeout);
     bool waitForStartup(unsigned long baudrate);
+    bool getHandle(const char* service, const char* characteristic, char handle[]);
     byte _pinWake_sw_7; //RN4020 pin 7
     byte _pinActive_12; //RN4020 pin 12
-    byte _pinCts_14;    //RN4020 pin 14
     byte _pinWake_hw_15;//RN4020 pin 15
     byte _pinEnPwr;
 };
